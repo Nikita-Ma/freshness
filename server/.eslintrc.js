d9cdea6ffdec6a26563768f4bee3a7752293474b
Nik <nikitama1hr@gmail.com>
2023-03-30 20:31:49 +0300

    initial server

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    semi: 0,
+    'comma-dangle': 0,
+    'prettier/prettier': 'error',
+    'function-component-definition': 0,
+    'arrow-body-style': 0,
+    'default-param-last': 0,
+    'object-shorthand': 0,
+    'no-unused-vars': 1,
+  },
+}
