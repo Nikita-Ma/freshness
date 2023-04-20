48fe5aaef9781f7eb3471de8b233491e826507a4
Nik <nikitama1hr@gmail.com>
2023-04-20 19:48:36 +0300

    [Backend] draft put & delete

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -12,20 +12,46 @@ const createProduct = asyncHandler(async (req, res) => {
   } = req.body
 
   const addNewProduct = await db.query(
-    'INSERT INTO product_data ( p_name,   p_id,p_date,p_desc, p_img,p_file) VALUES ($1, $2, $3, $4, $5,$6)',
+    'INSERT INTO product_data ( p_name, p_id, p_date, p_count, p_desc, p_img) VALUES ($1, $2, $3, $4, $5, $6)',
     [nameProduct, idProduct, dateProduct, countProduct, descProduct, file]
   )
+  return res.json('Product Create')
+})
 
-  console.log(
+const deleteProduct = asyncHandler(async (req, res, next) => {
+  const { nameProduct, idProduct } = req.body
+  if (nameProduct && idProduct) {
+    const deleteProductName = await db.query(
+      'DELETE FROM product_data WHERE p_name=$1',
+      [nameProduct]
+    )
+    return res.json('Product deleted (name&id)')
+  } else if (idProduct) {
+    const deleteProductId = await db.query(
+      'DELETE FROM product_data WHERE p_id=$1',
+      [idProduct]
+    )
+    return res.json('Product deleted(id)')
+  }
+  return res.sendStatus(400).json('Required input')
+})
+
+const updateProduct = asyncHandler(async (req, res, next) => {
+  const {
     nameProduct,
     idProduct,
     dateProduct,
     countProduct,
     descProduct,
-    file
+    file,
+  } = req.body
+
+  const updateProduct = await db.query(
+    'UPDATE  product_data SET ( p_name,   p_id,p_date,p_desc, p_img) VALUES ($1, $2, $3, $4, $5)',
+    [nameProduct, idProduct, dateProduct, countProduct, descProduct, file]
   )
-  console.log(addNewProduct)
-  res.sendStatus(401)
+
+  res.json('Updated')
 })
 
-module.exports = createProduct
+module.exports = { createProduct, deleteProduct, updateProduct }
--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const authMiddleware = require('../middleware/authMiddleware')
-const createProduct = require('../controllers/productController')
+const {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} = require('../controllers/productController')
 const routes = express.Router()
 // TODO: Create controllers product
 routes.route('/create').post(createProduct)
-routes.route('/delete').delete(createProduct)
-routes.route('/update').put(createProduct)
+routes.route('/delete').delete(deleteProduct)
+routes.route('/update').put(updateProduct)
 routes.route('/search').get(createProduct)
 
 module.exports = routes
